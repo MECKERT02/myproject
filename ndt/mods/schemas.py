c91d4694eddae56775901e5a5557b3fc87ecf29d
Mike Eckert <meckert2@madisoncollege.edu>
2024-06-03 18:19:35 -0500

    fghj

--- ndt/mods/schemas.py
+++ ndt/mods/schemas.py
@@ -6,42 +6,68 @@ from pydantic import BaseModel
 
 logger = logging.getLogger("schemas")
 
-def Config(BaseModel):
+class Config(BaseModel):
+    """
+    Config pydantic
+    """
     id: int
 
 class ItemBase(BaseModel):
+    """
+    Config pydantic
+    """
     title: str
     description: str | None = None
 
 
 class ItemCreate(ItemBase):
+    """
+    Config pydantic
+    """
     pass
 
 
 class Item(ItemBase):
+    """
+    Config pydantic
+    """
     id: int
     owner_id: int
 
     class Config:
+        """
+        Config pydantic
+        """
         from_attributes = True
 
 
 class UserBase(BaseModel):
+    """
+    Config pydantic
+    """
     email: str
 
 
 class UserCreate(UserBase):
+    """
+    Config pydantic
+    """
     password: str
 
 
 class User(BaseModel):
+    """
+    Config pydantic
+    """
     id: int
     username: str | None = None
-    hashed_password: str | None = None
     is_active: bool | None = None
     items: list[Item] = []
 
     class Config:
+        """
+        Config pydantic
+        """
         from_attributes = True
     #username: str | None = None
     #email: str | None = None
@@ -50,12 +76,21 @@ class User(BaseModel):
 
 
 class UserInDB(User):
+    """
+    Config pydantic
+    """
     hashed_password: str
 
 class Token(BaseModel):
+    """
+    Config pydantic
+    """
     access_token: str
     token_type: str
 
 
 class TokenData(BaseModel):
+    """
+    Config pydantic
+    """
     username: str | None = None
