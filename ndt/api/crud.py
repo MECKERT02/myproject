dc959aa01955e9ac102c09d8a82fbf9a2c361b6c
inf eck <infectious37@outlook.com>
2024-06-04 19:25:17 -0500

    eryt

--- ndt/api/crud.py
+++ ndt/api/crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 
-from . import models, schemas
-
+from . import schemas
+import ndt.mods.models as models
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
